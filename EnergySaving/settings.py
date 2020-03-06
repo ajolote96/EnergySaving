1d9bbc6c3e03f323b838a646a288cf6cf8f10c4c
= <=>
2020-03-05 20:34:03 -0600

    Prueba de despligue

--- EnergySaving/settings.py
+++ EnergySaving/settings.py
@@ -25,11 +25,7 @@ SECRET_KEY = 'k731q5h4xmrg!g1q#kb&#y&!9u6&%+m*-!#w0co5mk2vi#vsf$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-    '0.0.0.0',
-    'localhost',
-    '*',
-]
+ALLOWED_HOSTS = ['*',]
 
 
 # Application definition
@@ -45,6 +41,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -123,3 +120,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
--- EnergySaving/urls.py
+++ EnergySaving/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views as core_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', core_views.Home, name='Home'),
     path('EspRequest/', core_views.EspRequest, name='ESP')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
