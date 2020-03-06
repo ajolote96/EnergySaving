8624787d903f4e418ed3d10d7018b81d25c2c8a3
= <=>
2020-03-05 21:00:36 -0600

    Prueba de despligue

--- EnergySaving/urls.py
+++ EnergySaving/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path
 from core import views as core_views
 from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
