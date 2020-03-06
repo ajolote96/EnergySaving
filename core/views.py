57bcc03f92eaf8485d18ce6d4d79ebd9a075a4aa
= <=>
2020-03-05 21:54:51 -0600

    hola

--- core/views.py
+++ core/views.py
@@ -5,4 +5,4 @@ def Home(request):
     return render(request, "core/home.html")
 
 def EspRequest(request):
-    return HttpResponse('HOLA el proyecto va a salir a toda madre puto')
+    return HttpResponse("HOLA el proyecto va a salir a toda madre puto")
